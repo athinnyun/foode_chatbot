bb887419d582a2997c8dac8b66031276d308e814
athinnyun <39341245+athinnyun@users.noreply.github.com>
2018-06-29 17:16:07 -0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,9 @@
 # This is the initial file for the FoodE chatbot
 
 # Ideas for Chatbot functions:
-#
+
 # "Hello, how may I help you today? Would you like to:
-# 1. Find a place to eat
-# 2. Report an issue with our app
-# 3. Have your restaurant featured on our Facebook page
+# 1. Find a place to eat #Dennis
+# 2. Report an issue with our app #Dong
+# 3. Have your restaurant featured on our Facebook page #Tricia
+
